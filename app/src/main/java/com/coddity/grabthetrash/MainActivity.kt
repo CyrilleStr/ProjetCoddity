1fe0c9a839ff29c5f8a01732038c55af975c98f8
somia <somia.floret@outlook.com>
2022-04-07 18:07:20 +0200

    API maps

--- app/src/main/java/com/coddity/grabthetrash/MainActivity.kt
+++ app/src/main/java/com/coddity/grabthetrash/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_account
+                R.id.navigation_home, R.id.navigation_account, R.id.mapsFragment
             )
         )
         //setupActionBarWithNavController(navController, appBarConfiguration)
