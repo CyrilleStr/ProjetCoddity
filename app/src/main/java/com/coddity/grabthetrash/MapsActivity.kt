9b6c3343c65c111ee69aeddd645c0d01ec8b0a0c
somia <somia.floret@outlook.com>
2022-04-22 16:08:52 +0200

    delete all fragments and add trash icon

--- app/src/main/java/com/coddity/grabthetrash/MapsActivity.kt
+++ app/src/main/java/com/coddity/grabthetrash/MapsActivity.kt
@@ -1,21 +1,27 @@
 package com.coddity.grabthetrash
 
 
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
 import android.location.Location
 import android.os.Bundle
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.*
 
 
 internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
@@ -59,6 +65,70 @@ internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap
             false
         })
     }
+    private fun placeMarkerOnMap(location: LatLng, title: String) {
+        // 1
+        val markerOptions = MarkerOptions().position(location)
+        val icon = BitmapDescriptorFactory.fromBitmap(resizeMapIcons("trash",100,100))
+        markerOptions.title(title)
+        markerOptions.icon(icon)
+        mMap.addMarker(markerOptions)
+    }
+    private fun resizeMapIcons(iconName: String?, width: Int, height: Int): Bitmap {
+        val imageBitmap = BitmapFactory.decodeResource(
+            resources, resources.getIdentifier(
+                iconName, "drawable",
+                packageName
+            )
+        )
+        return Bitmap.createScaledBitmap(imageBitmap, width, height, false)
+    }
+    fun getBitmapDescriptorFromVector(context: Context, @DrawableRes vectorDrawableResourceId: Int): BitmapDescriptor? {
+
+        val vectorDrawable = ContextCompat.getDrawable(context, vectorDrawableResourceId)
+        val bitmap = Bitmap.createBitmap(vectorDrawable!!.intrinsicWidth, vectorDrawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        vectorDrawable.setBounds(0, 0, canvas.width, canvas.height)
+        vectorDrawable.draw(canvas)
+
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
+    fun getRandomLocation(point: Location, radius: Int): LatLng {
+        val randomPoints: MutableList<LatLng> = ArrayList()
+        val randomDistances: MutableList<Float> = ArrayList()
+        val myLocation = Location("")
+        myLocation.latitude = point.latitude
+        myLocation.longitude = point.longitude
+
+        //This is to generate 10 random points
+        for (i in 0..9) {
+            val x0 = point.latitude
+            val y0 = point.longitude
+            val random = Random()
+
+            // Convert radius from meters to degrees
+            val radiusInDegrees = (radius / 111000f).toDouble()
+            val u: Double = random.nextDouble()
+            val v: Double = random.nextDouble()
+            val w = radiusInDegrees * Math.sqrt(u)
+            val t = 2 * Math.PI * v
+            val x = w * Math.cos(t)
+            val y = w * Math.sin(t)
+
+            // Adjust the x-coordinate for the shrinking of the east-west distances
+            val new_x = x / Math.cos(y0)
+            val foundLatitude = new_x + x0
+            val foundLongitude = y + y0
+            val randomLatLng = LatLng(foundLatitude, foundLongitude)
+            randomPoints.add(randomLatLng)
+            val l1 = Location("")
+            l1.latitude = randomLatLng.latitude
+            l1.longitude = randomLatLng.longitude
+            randomDistances.add(l1.distanceTo(myLocation))
+        }
+        //Get nearest point to the centre
+        val indexOfNearestPointToCentre = randomDistances.indexOf(Collections.min(randomDistances))
+        return randomPoints[indexOfNearestPointToCentre]
+    }
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1
         private const val REQUEST_CHECK_SETTINGS = 2
@@ -82,12 +152,8 @@ internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap
         setUpMap()
     }
 
-    fun onMarkerClick(p0: Marker?) {
-        false
-    }
-
     override fun onMarkerClick(p0: Marker): Boolean {
-        TODO("Not yet implemented")
+        return false
     }
 
     private fun setUpMap() {
@@ -106,8 +172,19 @@ internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap
             // 3
             if (location != null) {
                 lastLocation = location
-                val currentLatLng = LatLng(location.latitude, location.longitude)
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+                val currentLatLng = LatLng(lastLocation.latitude, lastLocation.longitude)
+
+                for (i in 1..10) {
+                    println(i)
+                    val rand_loc = getRandomLocation(location, 500)
+                    val poubelleMarker = LatLng(rand_loc.latitude, rand_loc.longitude)
+                    val title = "Poubelle $i"
+                    placeMarkerOnMap(rand_loc, title)
+                }
+
+
+
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 18f))
             }
         }
     }
