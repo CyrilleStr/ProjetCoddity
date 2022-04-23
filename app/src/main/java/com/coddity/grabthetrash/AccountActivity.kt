87f3a2a621fea07dc7801ece3c22973354259315
Samy <smchouit@gmail.com>
2022-04-24 01:31:44 +0200

    Login functions

--- app/src/main/java/com/coddity/grabthetrash/AccountActivity.kt
+++ app/src/main/java/com/coddity/grabthetrash/AccountActivity.kt
@@ -4,12 +4,26 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import okhttp3.*
+import web.PostString
 
 class AccountActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
+
+        /** Login form function **/
+        var button = findViewById<Button>(R.id.loginButton)
+        var text = findViewById<TextView>(R.id.textView)
+
+        button.setOnClickListener {
+            //text.text = client2.post()
+            login()
+        }
 
         /** Setup bottom navigation **/
         // Get View
@@ -33,5 +47,29 @@ class AccountActivity : AppCompatActivity() {
             }
             false
         })
+    }
+
+    fun login():Unit{
+        val username = "a"//username.text
+        val passwd = "a"//password.text
+        if(username.isNullOrEmpty()){
+            println("Username Should not be empty")
+            showinfo("Username Should not be empty")
+
+        } else{
+            val url = "https://projetcoddityserverside.herokuapp.com/auth/token"
+            val map: HashMap<String, String> = hashMapOf("username" to "sosacy", "password" to "calottedetesmorts")
+
+            PostString().authenticate(url, map)
+
+            val i = Intent(this,HomeActivity::class.java)
+            startActivity(i)
+            finish()
+        }
+    }
+
+    fun showinfo(msg:String){
+        println("inside showInfo")
+        Toast.makeText(applicationContext,"$msg",Toast.LENGTH_SHORT).show()
     }
 }
